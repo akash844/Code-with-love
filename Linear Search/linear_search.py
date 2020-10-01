2490257d2d3c899125e7914244aa123f4e52e0c7
Akash Garg <45414700+akash844@users.noreply.github.com>
2020-10-01 21:36:00 +0530

    added_linear_search.py

---
+++ Linear Search/linear_search.py
@@ -0,0 +1,19 @@
+# If x is present then return its location, 
+# otherwise return -1 
+  
+def search(arr, n, x): 
+  
+    for i in range (0, n): 
+        if (arr[i] == x): 
+            return i; 
+    return -1; 
+  
+# Driver Code 
+arr = [ 52, 7, 41, 12, 8 ]; 
+x = 12; 
+n = len(arr); 
+result = search(arr, n, x) 
+if(result == -1): 
+    print("Element is not present in array") 
+else: 
+    print("Element is present at index", result);
